d14bed161208b42f92f34f866dad825d5d97efec
mine7272 <31873187@naver.com>
2022-03-31 13:35:56 +0900

    hihi

--- app.py
+++ app.py
@@ -5,7 +5,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Test 2"
+    return "Test 111"
+ 
+@app.route('/userLogin', methods = ['POST'])
+def userLogin():
+    user = request.get_json()#json 데이터를 받아옴
+    return jsonify(user)# 받아온 데이터를 다시 전송
+ 
+@app.route('/hihi/<text>')
+def hihi(text):
+    return jsonify({"hihi":text})
+ 
+ 
+if __name__ == "__main__":
+    app.run()
+
 
 run_with_ngrok(app)
 app.run()
