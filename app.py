b2a098a958ac08cf70467afcb84ae2506c6a83c2
mine7272 <31873187@naver.com>
2022-03-31 23:08:53 +0900

    up

--- app.py
+++ app.py
@@ -5,16 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return "Test 111"
- 
-@app.route('/userLogin', methods = ['POST'])
-def userLogin():
-    user = request.get_json()#json 데이터를 받아옴
-    return jsonify(user)# 받아온 데이터를 다시 전송
- 
+
 @app.route('/hihi/<text>')
 def hihi(text):
     return jsonify({"hihi":text})
- 
  
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
