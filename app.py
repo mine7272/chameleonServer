bddb89623ecc72d7ea49bf008b068069034dcb42
mine7272 <31873187@naver.com>
2022-04-14 22:16:14 +0900

    .

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify
+from flask_restx import Api,Resource,reqparse
 
 app = Flask(__name__)
+
+api = Api(app, version='1.0', title='API 문서', description='Swagger 문서', doc="/api-docs")
 
 @app.route('/')
 def hello_world():
