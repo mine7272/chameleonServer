9a587f4318b9755c84c8e48ab8d402b436947a05
mine7272 <31873187@naver.com>
2022-05-04 12:18:22 +0900

    .

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask, request
+from flask_cors import CORS
+from flask_restx import Api, Resource, reqparse
+from flask_mysqldb import MySQL
+from werkzeug.utils import secure_filename
+
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+CORS(app)
+app.config['UPLOAD_FOLDER'] = '/'
 
-@app.route('/')
-def hello_world():
-    return "Test 111"
+api = Api(app, version='1.0', title='chameleon ios api', description='Swagger 문서', doc="/api-docs")
+ns = api.namespace('test', description='조회 API')
 
-@app.route('/hihi/<text>')
-def hihi(text):
-    return jsonify({"hihi":text})
- 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
 
+@ns.route('/')
+class Test(Resource): 
+    def get(self): 
+        return 'Hello World!'
+
+@app.route('/fileupload', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return '파일 업로드 성공!'
+
+if __name__ == '__main__': 
+    app.run(host='0.0.0.0', port=5000,debug=True)
