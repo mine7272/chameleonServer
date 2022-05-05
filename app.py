d4353995d765b752c2c3d7d3befe3cc22d8b2e76
mine7272 <31873187@naver.com>
2022-05-05 14:47:41 +0900

    1212

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, request, jsonify
+from urllib import response
+from flask import Flask, request, jsonify, json
 from flask_cors import CORS
 from flask_restx import Api, Resource, reqparse
 from werkzeug.utils import secure_filename
@@ -7,6 +8,11 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 CORS(app)
 app.config['UPLOAD_FOLDER'] = '/'
+
+api =Api(app, version='1.0', title='API 문서', description='Swagger 문서', doc="/api-docs")
+test_api = api.namespace('test', description='조회 API')
+
+
 
 @app.route('/server-test')
 def home():
@@ -20,6 +26,16 @@ def upload_file():
         return jsonify({"result": "upload success"})
     else :
         return jsonify({"result": "upload fail"})
+    
+@app.route('/test', methods = ['POST'])
+def upload_test():
+    jsonData=request.get_json()
+    print(jsonData)
+    return {
+        "result": "ok",
+        "message": "request"
+    }
+    
 
 if __name__ == '__main__': 
     app.run(host='0.0.0.0', port=5000 , debug="True")
