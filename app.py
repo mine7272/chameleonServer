862533b58ca1330b794120b885b307ff73487308
mine7272 <31873187@naver.com>
2022-05-14 22:23:24 +0900

    11

--- app.py
+++ app.py
@@ -26,7 +26,8 @@ def upload_file():
     onlynum=request.headers.get('authorization')
     os.makedirs("../database/"+onlynum)
     f.save("../database/{}/".format(onlynum) +secure_filename(f.filename))
-    subprocess.run("cd ../ && python src/classifier.py --type photo --key "+onlynum, shell=True)
+    #subprocess.run("cd ../ && python src/classifier.py --type photo --key "+onlynum, shell=True)
+    os.system("cd ../ && python src/classifier.py --type photo --key "+onlynum)
     return jsonify({"result":"ok"})
      
 @app.route('/version', methods = ['GET'])
