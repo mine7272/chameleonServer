de45f4ef6364d1b066f8f63ba659117aaac3b10a
mine7272 <31873187@naver.com>
2022-05-14 23:38:20 +0900

    42

--- app.py
+++ app.py
@@ -26,11 +26,8 @@ def upload_file():
     onlynum=request.headers.get('authorization')
     os.makedirs("../database/"+onlynum)
     f.save("../database/{}/".format(onlynum) +secure_filename(f.filename))
-    croplist(onlynum)
-    return jsonify({"result":"ok"})
-
-def croplist(onlynum):
     os.system("cd ../ && python src/classifier.py --type photo --key "+onlynum)
+    return jsonify({"result":"ok"})
      
 @app.route('/version', methods = ['GET'])
 def version():
