ade2aa74a66cdd499cb1e3ee6d35bd4673d0bee4
mine7272 <31873187@naver.com>
2022-05-17 12:42:12 +0900

    tt

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def faces():
     if request.method == 'GET':
         onlynum=request.headers.get('authorization')
         os.system("cd ../ && python src/classifier.py --type photo --key "+onlynum)
-        os.system("cd ../database/{}/tmp/LQ_faces && cp * ../../../../chameleonServer/static/img".format(onlynum))
+        os.system("cd database/{}/tmp/LQ_faces && cp * ../../../../chameleonServer/static/img".format(onlynum))
         
         base_path = "/home/yona/chameleon_project/chameleonServer/static/img"
         files = os.listdir(base_path)
